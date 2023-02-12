e1c8eab83f99fe9e8580d72b076a211d7b2c3347
Mariam <mmariam.gigauri@gmail.com>
2023-02-12 19:31:12 +0400

    Info added to Localstorage

--- script.js
+++ script.js
@@ -10,46 +10,68 @@ function savePersonalInfo() {
     const tel = document.querySelector('#tel').value;
     const aboutMe = document.querySelector('#aboutMe').value;
 
+    if((tel.startsWith("+995")) && tel.length > 12 && tel.length <14){
+        document.querySelector('#tel').parentElement.classList.remove('validation-error');
+        document.querySelector('#tel').parentElement.classList.add('validation-done');
+    } else {
+        document.querySelector('#tel').parentElement.classList.add('validation-error');
+        document.querySelector('#tel').parentElement.classList.remove('validation-done');
+    }
+
     if(name.length >= 2) {
         document.querySelector('#name').parentElement.classList.remove('validation-error');
         document.querySelector('#name').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#name').parentElement.classList.add('validation-error');
+        document.querySelector('#name').parentElement.classList.remove('validation-done');
     }
     if(surname.length >= 2) {
         document.querySelector('#surname').parentElement.classList.remove('validation-error');
         document.querySelector('#surname').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#surname').parentElement.classList.add('validation-error');
+        document.querySelector('#surname').parentElement.classList.remove('validation-done');
     }
-    if(email.lenght >= 1 && (email.indexOf(domainName) + domainName.length) == domainName.length) {
+    if(email.length >= 1 && (email.indexOf(domainName) + domainName.length) == email.length) {
         document.querySelector('#email').parentElement.classList.remove('validation-error');
         document.querySelector('#email').parentElement.classList.add('validation-done');
     } else {
          document.querySelector('#email').parentElement.classList.add('validation-error');
+         document.querySelector('#email').parentElement.classList.remove('validation-done');
     }
-    
-    const data = { name, surname, email,  tel }
+
+    const data = { name, surname, email,  tel, aboutMe }
+    localStorage.setItem('registrationDate', JSON.stringify(data));
     console.log(data);
 }
 function saveEducation() {
     const univeristy = document.querySelector('#university').value;
     const details = document.querySelector('#details').value;
+    const degree = document.querySelector('#degree').value;
     
-
+    if(degree == "")  {
+        document.querySelector('#degree').parentElement.classList.add('validation-error');
+        document.querySelector('#degree').parentElement.classList.remove('validation-done');
+    } else {
+        document.querySelector('#degree').parentElement.classList.remove('validation-error');
+        document.querySelector('#degree').parentElement.classList.add('validation-done');
+    }
     if(univeristy.length >= 2) {
         document.querySelector('#university').parentElement.classList.remove('validation-error');
         document.querySelector('#university').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#university').parentElement.classList.add('validation-error');
+        document.querySelector('#university').parentElement.classList.remove('validation-done');
     }
     if(details.length > 0) {
         document.querySelector('#details').parentElement.classList.remove('validation-error');
         document.querySelector('#details').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#details').parentElement.classList.add('validation-error');
+        document.querySelector('#details').parentElement.classList.remove('validation-done');
     }
-    const data = { name, surname, email,  tel }
+
+    const data = { univeristy, degree, details }
     console.log(data);
 }
 function saveExperience() {
@@ -62,19 +84,32 @@ function saveExperience() {
         document.querySelector('#position').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#position').parentElement.classList.add('validation-error');
+        document.querySelector('#position').parentElement.classList.remove('validation-done');
     }
     if(company.length >= 2) {
         document.querySelector('#company').parentElement.classList.remove('validation-error');
         document.querySelector('#company').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#company').parentElement.classList.add('validation-error');
+        document.querySelector('#company').parentElement.classList.remove('validation-done');
     }
     if(description.length >= 1) {
         document.querySelector('#description').parentElement.classList.remove('validation-error');
         document.querySelector('#description').parentElement.classList.add('validation-done');
     } else {
         document.querySelector('#description').parentElement.classList.add('validation-error');
+        document.querySelector('#description').parentElement.classList.remove('validation-done');
     }
-    const data = { position, company, start, end, description }
+    const data = { position, company, description }
     console.log(data);
 }   
+
+window.onload = function() {
+    const data = JSON.parse(localStorage.getItem('registrationDate'));
+
+    for (const [key, value] of Object.entries(data)) {
+        console.log(`${key}: ${value}`);
+        document.querySelector(`#${key}`).value = value;
+        document.querySelector(`#${key}Result`).textContent = value;
+    }
+}
