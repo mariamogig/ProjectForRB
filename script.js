46a50b836a1a20059799d783bdc80c557c63c39c
Mariam <mmariam.gigauri@gmail.com>
2023-02-13 21:39:27 +0400

    all pages info merged in localstorage

--- script.js
+++ script.js
@@ -38,50 +38,17 @@ function savePersonalInfo() {
     } else {
          document.querySelector('#email').parentElement.classList.add('validation-error');
          document.querySelector('#email').parentElement.classList.remove('validation-done');
-    }
-
-    const data = { name, surname, email,  tel, aboutMe }
-    localStorage.setItem('registrationDate', JSON.stringify(data));
-    console.log(data);
-}
-function saveEducation() {
-    const univeristy = document.querySelector('#university').value;
-    const details = document.querySelector('#details').value;
-    const degree = document.querySelector('#degree').value;
-    const date = document.querySelector('#date').value;
+    }   
     
-    if(date == "") {
-        document.querySelector('#date').parentElement.classList.add('validation-error');
-        document.querySelector('#date').parentElement.classList.remove('validation-done');
-    } else {
-        document.querySelector('#date').parentElement.classList.remove('validation-error');
-        document.querySelector('#date').parentElement.classList.add('validation-done');
-    }
-    if(degree == "")  {
-        document.querySelector('#degree').parentElement.classList.add('validation-error');
-        document.querySelector('#degree').parentElement.classList.remove('validation-done');
-    } else {
-        document.querySelector('#degree').parentElement.classList.remove('validation-error');
-        document.querySelector('#degree').parentElement.classList.add('validation-done');
-    }
-    if(univeristy.length >= 2) {
-        document.querySelector('#university').parentElement.classList.remove('validation-error');
-        document.querySelector('#university').parentElement.classList.add('validation-done');
-    } else {
-        document.querySelector('#university').parentElement.classList.add('validation-error');
-        document.querySelector('#university').parentElement.classList.remove('validation-done');
-    }
-    if(details.length > 0) {
-        document.querySelector('#details').parentElement.classList.remove('validation-error');
-        document.querySelector('#details').parentElement.classList.add('validation-done');
-    } else {
-        document.querySelector('#details').parentElement.classList.add('validation-error');
-        document.querySelector('#details').parentElement.classList.remove('validation-done');
-    }
-
-    const data = { univeristy, degree, details, date }
-    localStorage.setItem('registrationDate', JSON.stringify(data));
+    const savedInfo = JSON.parse(localStorage.getItem('registrationDate'));
+    const data = { name, surname, email,  tel, aboutMe }
+    const mergeObj = {...savedInfo, ...data};
+    localStorage.setItem('registrationDate', JSON.stringify(mergeObj));
     console.log(data);
+    
+    if(email.length >= 1 && (email.indexOf(domainName) + domainName.length) == email.length && surname.length >= 2 && name.length >= 2 && (tel.startsWith("+995")) && tel.length > 12 && tel.length <14)  {
+        document.location.href = "experience.html";
+    }  
 }
 function saveExperience() {
     const position = document.querySelector('#position').value;
@@ -125,23 +92,70 @@ function saveExperience() {
         document.querySelector('#description').parentElement.classList.add('validation-error');
         document.querySelector('#description').parentElement.classList.remove('validation-done');
     }
+
+    const savedInfo = JSON.parse(localStorage.getItem('registrationDate'));
     const data = { position, company, description, start, end}
-    localStorage.setItem('registrationDate', JSON.stringify(data));
+    const mergeObj = {...savedInfo, ...data};
+    localStorage.setItem('registrationDate', JSON.stringify(mergeObj));
     console.log(data);
 }   
-
-window.onload = function() {
+function saveEducation() {
+    const univeristy = document.querySelector('#university').value;
+    const details = document.querySelector('#details').value;
+    const degree = document.querySelector('#degree').value;
+    const date = document.querySelector('#date').value;
+    
+    if(date == "") {
+        document.querySelector('#date').parentElement.classList.add('validation-error');
+        document.querySelector('#date').parentElement.classList.remove('validation-done');
+    } else {
+        document.querySelector('#date').parentElement.classList.remove('validation-error');
+        document.querySelector('#date').parentElement.classList.add('validation-done');
+    }
+    if(degree == "")  {
+        document.querySelector('#degree').parentElement.classList.add('validation-error');
+        document.querySelector('#degree').parentElement.classList.remove('validation-done');
+    } else {
+        document.querySelector('#degree').parentElement.classList.remove('validation-error');
+        document.querySelector('#degree').parentElement.classList.add('validation-done');
+    }
+    if(univeristy.length >= 2) {
+        document.querySelector('#university').parentElement.classList.remove('validation-error');
+        document.querySelector('#university').parentElement.classList.add('validation-done');
+    } else {
+        document.querySelector('#university').parentElement.classList.add('validation-error');
+        document.querySelector('#university').parentElement.classList.remove('validation-done');
+    }
+    if(details.length > 0) {
+        document.querySelector('#details').parentElement.classList.remove('validation-error');
+        document.querySelector('#details').parentElement.classList.add('validation-done');
+    } else {
+        document.querySelector('#details').parentElement.classList.add('validation-error');
+        document.querySelector('#details').parentElement.classList.remove('validation-done');
+    }
+    
+    const savedInfo = JSON.parse(localStorage.getItem('registrationDate'));
+    const data = { univeristy, degree, details, date };
+    const mergeObj = {...savedInfo, ...data};
+    localStorage.setItem('registrationDate', JSON.stringify(mergeObj));
+    console.log(data);
+}
+document.onload = function() {
     const data = JSON.parse(localStorage.getItem('registrationDate'));
 
     for (const [key, value] of Object.entries(data)) {
         console.log(`${key}: ${value}`);
-        document.querySelector(`#${key}`).value = value;
-        document.querySelector(`#${key}Result`).textContent = value;
+        if(document.querySelector(`#${key}`)?.length > 0){
+            document.querySelector(`#${key}`).value = value;
+        }
+        if(document.querySelector(`#${key}Result`)?.length > 0) {
+            document.querySelector(`#${key}Result`).textContent = value;
+        }
     }
 }
 
 function addInputs() {
-    const container = document.getElementById('test');
+    const container = document.getElementById('addInput');
     let input = document.createElement('input');
     input.placeholder = "სასწავლებელი";
     container.appendChild(input);
